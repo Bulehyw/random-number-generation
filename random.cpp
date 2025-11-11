71bb4ffd7e25258ae3a1e03c76191d1dd00ac4b4
Bulehyw <89201108+Bulehyw@users.noreply.github.com>
2025-11-11 13:05:15 +0800

    Add files via upload
    生成一个随机数

---
+++ random.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include<random>
+using namespace std;
+
+void random_int() {
+    //生成随机数
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(1, 100); //生成1到100之间的随机数
+    int num = dis(gen);
+    cout << "Random number: " << num << endl;
+
+}
+
+
+
+int main()
+{
+    //输入
+    int n=1;
+
+
+    while (n==1) {
+        random_int();
+        cout << "是否继续？"<<endl;
+        cin >> n;
+    }
+    
+    return 0;
+}
+
